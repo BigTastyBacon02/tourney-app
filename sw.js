171c9ff0b6598b70fe1c11440b0695b197ec3d46
BigTastyBacon02 <peterguenter4@gmx.de>
2025-10-01 18:46:53 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'promi-duell-cache-v1';
+const ASSETS = ['.', './index.html', './manifest.webmanifest', './icon-180.png', './assets/placeholder.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
